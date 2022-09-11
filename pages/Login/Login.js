7bccf88b43f707e2865b5cd4fbd1db840c3e006d
h4lo343 <92515186+h4lo343@users.noreply.github.com>
2022-09-11 17:44:24 +1000

    create login page

---
+++ pages/Login/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export const Login = () => {
+  return (
+    <View>
+      <Text>This is login page</Text>
+    </View>
+  )
+}
