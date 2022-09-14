167fed6b5c392baca1631ef3dc863ab7eeaf9b94
h4lo343 <92515186+h4lo343@users.noreply.github.com>
2022-09-14 14:11:58 +1000

    finished login page

--- pages/Login/Login.js
+++ pages/Login/Login.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, CheckBox} from 'react-native';
+import { StyleSheet, Text, View, Image, CheckBox } from 'react-native';
 import { Link } from 'react-router-native';
 import { Box, FormControl, Input, WarningOutlineIcon, Stack, MaterialIcons, Pressable, Icon, Button, Checkbox } from 'native-base';
+import { ScaledImage } from '../../components';
 
 export const Login = () => {
   const [show, setShow] = React.useState(false);
@@ -16,29 +17,41 @@ export const Login = () => {
 
 
       <Box alignItems="center" style={styles.inputBox}>
-        <Input variant="underlined" placeholder="Username, Email or Phone Number"  fontSize={14}/>
+        <Input variant="underlined" placeholder="Username, Email or Phone Number" fontSize={14} />
         <Input variant='underlined'
           placeholder='password'
           type={show ? "text" : "password"}
           fontSize={14}
-          InputRightElement={ 
-             <Button size="xs" rounded="none" w="1/6" h="full" onPress={handleClick}>
-            {show ? "Hide" : "Show"}
-          </Button>
-           }
+          InputRightElement={
+            <Button size="xs" rounded="none" w="1/6" h="full" onPress={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          }
         />
       </Box>
 
-      <View style={{marginTop:30, justifyContent:"space-between", flexDirection:"row"}}>
-         <Checkbox value="two"><Text style={{fontSize:14}}>Remember Me</Text></Checkbox>
-         <Link to='/reset'><Text>Forgot Password?</Text></Link>
+      <View style={{ marginTop: 30, justifyContent: "space-between", flexDirection: "row" }}>
+        <Checkbox value="two"><Text style={{ fontSize: 14 }}>Remember Me</Text></Checkbox>
+        <Link to='/reset'><Text>Forgot Password?</Text></Link>
       </View>
 
-      <Box alignItems="center" style={{marginTop: 40}}>
-        <Button style={styles.loginButton}>Log in</Button>
+      <Box alignItems="center" style={{ marginTop: 40 }}>
+        <Button style={styles.loginButton}><Text style={{ fontWeight: 'bold', fontSize: 20 }}>Log in</Text></Button>
+
         <Text>OR</Text>
-      </Box>    
-      
+
+        <Button style={styles.loginSpotify}>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Text style={{ fontWeight: 'bold', color: "white", fontSize: 20 }}>Log In With</Text>
+            <Image style={{ width: 80, resizeMode: "contain", right: -10 }} source={require('../../assets/imgs/spotify.jpg')} />
+          </View>
+        </Button>
+      </Box>
+
+      <Box alignItems={"center"} style={{ bottom: -100 }} flexDirection="row" justifyContent={"center"}>
+        <Text>Don't have an account?</Text>
+        <Link to="/register"><Text style={{color:"black"}}>  Sign in</Text></Link>
+      </Box>
 
     </View>
   )
@@ -60,7 +73,12 @@ const styles = StyleSheet.create({
   },
   loginButton: {
     backgroundColor: "#e4b1a5",
-    width: "100%"
+    width: "100%",
+  },
+  loginSpotify: {
+    width: "100%",
+    backgroundColor: "#343341",
+    height: 50
   }
 
 })
