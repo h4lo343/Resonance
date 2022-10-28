3a2c50cde83351967993fd6dbcc290e02c12282c
Arachidonic <jiayanc1@student.unimelb.edu.au>
2022-10-28 12:51:32 +1100

    link oages and clean import

--- frontend/pages/Navigation/StackNavigator.js
+++ frontend/pages/Navigation/StackNavigator.js
@@ -8,8 +8,6 @@ import { Login } from '../Login/Login';
 import { Register } from '../Register/Register';
 import { PasswordReset } from '../PasswordReset/PasswordReset';
 import { UserProfile } from '../UserProfile/UserProfile';
-import { EditProfile } from '../EditProfile/EditProfile';
-import { MapViewPage } from '../MapViewPage/MapViewPage';
 import { AnotherUserProfile } from '../AnotherUserProfile/AnotherUserProfile';
 
 const Stack = createNativeStackNavigator();
@@ -17,14 +15,12 @@ const Stack = createNativeStackNavigator();
 const StackNavigator = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="DrawerNavigator" component={DrawerNavigator} options={{ headerShown: false }}/> 
+      {/* <Stack.Screen name="DrawerNavigator" component={DrawerNavigator} options={{ headerShown: false }}/>  */}
       <Stack.Screen name="Login" component={Login} options={{ headerShown: false }}/>
       <Stack.Screen name="Register" component={Register} />
       <Stack.Screen name="PasswordReset" component={PasswordReset} />
-      {/* <Stack.Screen name="DrawerNavigator" component={DrawerNavigator} options={{ headerShown: false }}/> */}
-      <Stack.Screen name="MapViewPage" component={MapViewPage} />
+      <Stack.Screen name="DrawerNavigator" component={DrawerNavigator} options={{ headerShown: false }}/>
       <Stack.Screen name="AnotherUserProfile" component={AnotherUserProfile} options={{ headerShown: false }} />
-      <Stack.Screen name="EditProfile" component={EditProfile} />
       <Stack.Screen name="UserProfile" component={UserProfile} options={{ headerShown: false }}/>
     </Stack.Navigator>
   );
