b54332d2178b6717a359bc994c73e1555d8265f6
Arachidonic <jiayanc1@student.unimelb.edu.au>
2022-10-28 12:48:59 +1100

    Revert "add mapview to login page"
    This reverts commit aae8dd0df34b7d8901cb3e8c43ebae9ed27febc9.

--- frontend/pages/Navigation/StackNavigator.js
+++ frontend/pages/Navigation/StackNavigator.js
@@ -7,25 +7,25 @@ import DrawerNavigator from './DrawerNavigator';
 import { Login } from '../Login/Login';
 import { Register } from '../Register/Register';
 import { PasswordReset } from '../PasswordReset/PasswordReset';
+import { UserProfile } from '../UserProfile/UserProfile';
 import { EditProfile } from '../EditProfile/EditProfile';
+import { MapViewPage } from '../MapViewPage/MapViewPage';
 import { AnotherUserProfile } from '../AnotherUserProfile/AnotherUserProfile';
-import { NearbyMusicDisplay } from '../../components/NearbyMusicDisplay';
 
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
   return (
     <Stack.Navigator>
-      {/* <Stack.Screen name="DrawerNavigator" component={DrawerNavigator} options={{ headerShown: false }}/>  */}
-      <Stack.Screen name="NearbyMusicDisplay" component={NearbyMusicDisplay} options={{ headerShown: false }}/>
+      <Stack.Screen name="DrawerNavigator" component={DrawerNavigator} options={{ headerShown: false }}/> 
       <Stack.Screen name="Login" component={Login} options={{ headerShown: false }}/>
       <Stack.Screen name="Register" component={Register} />
       <Stack.Screen name="PasswordReset" component={PasswordReset} />
-      <Stack.Screen name="DrawerNavigator" component={DrawerNavigator} />
-      {/* <Stack.Screen name="MapViewPage" component={MapViewPage} /> */}
+      {/* <Stack.Screen name="DrawerNavigator" component={DrawerNavigator} options={{ headerShown: false }}/> */}
+      <Stack.Screen name="MapViewPage" component={MapViewPage} />
       <Stack.Screen name="AnotherUserProfile" component={AnotherUserProfile} options={{ headerShown: false }} />
       <Stack.Screen name="EditProfile" component={EditProfile} />
-      {/* <Stack.Screen name="UserProfile" component={UserProfile} options={{ headerShown: false }}/> */}
+      <Stack.Screen name="UserProfile" component={UserProfile} options={{ headerShown: false }}/>
     </Stack.Navigator>
   );
 };
