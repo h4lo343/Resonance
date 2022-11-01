e4556df4a01751fdcdfa04c90c152ea7c3899721
Arachidonic <jiayanc1@student.unimelb.edu.au>
2022-11-01 23:55:07 +1100

    add component in drawer
    add log out page in drawer navigator
    change map title
    add comment and clear unused code on login page

--- frontend/pages/Navigation/DrawerNavigator.js
+++ frontend/pages/Navigation/DrawerNavigator.js
@@ -3,15 +3,17 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { MapViewPage } from '../MapViewPage/MapViewPage';
 import { Moment } from '../Moment/Moment';
 import { UserProfile } from '../UserProfile';
+import { Login } from '../Login';
 
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () => {
     return (
       <Drawer.Navigator>
-        <Drawer.Screen name="Map" component={MapViewPage} />
+        <Drawer.Screen name="Map" component={MapViewPage} options={{title: 'shake to discover nearby music'}}/>
         <Drawer.Screen name="Profile" component={UserProfile} />
         <Drawer.Screen name="Moment" component={Moment} />
+        <Drawer.Screen name="Log Out" component={Login} options={{ headerShown: false }}/>
       </Drawer.Navigator>
     );
   };
